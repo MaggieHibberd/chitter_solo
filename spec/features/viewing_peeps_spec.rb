cad3c9d1382bbe69215caa74c36b927626cd543f
Maggie Hibberd <maggiehibberd@gmail.com>
2021-04-07 15:39:42 +0100

    Viewing a peep test refactored & now connecting with .create method

--- spec/features/viewing_peeps_spec.rb
+++ spec/features/viewing_peeps_spec.rb
@@ -1,25 +1,25 @@
 require 'pg'
 
-#   feature 'visiting the homepage' do
-#     scenario 'the page title is visible' do
-#     visit('/')
-#     expect(page).to have_content 'Peeps'
-#   end
-# end
+
+feature 'visiting the homepage' do
+  scenario 'the page title is visible' do
+    visit('/')
+    expect(page).to have_content 'Peeps'
+  end
+end
 
 feature 'Viewing a peep' do 
   scenario 'visiting chitters peep route to view peeps' do
-    connection = PG.connect(dbname: 'chitter_management_test')
 
-    connection.exec("INSERT INTO peeps VALUES(1, 'My first peep');")
-      # connection.exec("INSERT INTO peeps (content) VALUES ('Get to the chopper');")
-      # connection.exec("INSERT INTO peeps (content) VALUES ('We have landed');")
+    ChitterWeb.create(peep: 'My first peep')
+    ChitterWeb.create(peep:'Get to the chopper') 
+    ChitterWeb.create(peep: 'We have landed') 
 
     visit('/peeps')
 
     expect(page).to have_content 'My first peep'
-    # expect(page).to have_content 'Get to the chopper'
-    # expect(page).to have_content 'We have landed'
+    expect(page).to have_content 'Get to the chopper'
+    expect(page).to have_content 'We have landed'
   end 
 end
 
